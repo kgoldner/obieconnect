84d5a5a3e5bd770185f3a5f61bd5e1a96f703fcb
Yihui Chen <uninterrupted@gmail.com>
2012-04-15 23:05:13 -0400

    Created forms.py and urls.py
    Helpful links:
    https://docs.djangoproject.com/en/dev/topics/auth/
    http://eli.thegreenplace.net/2011/07/17/django-sessions-part-iii-user-au
    thentication/
    http://www.b-list.org/weblog/2006/sep/02/django-tips-user-registration/

---
+++ forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from django.forms.models import ModelForm
+
+# User registration form
+
+# Login form
--- views.py
+++ views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+from obieconnect.models import ObieConnectProfile, Course, Department, Professor
+
+# If user is logged in, take to personal profile
+# Else display login form AND link to registration form
+
+# Department view 
+
+# Professor view
+
+# Course page view
+    # Comments
+    # Include list of users?
+
