0019eface936d1b507a76d690d3417d9ae909c24
Yihui Chen <uninterrupted@gmail.com>
2012-04-15 21:54:19 -0400

    admin.py file just for funsies

---
+++ admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from bookshelf.models import Department, Professor, Course
+
+class DepartmentAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("shortname",)}
+    
+class ProfessorAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("firstname" + "-" + "lastname",)}
+
+class CourseAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("department" + "level",)}
+    
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Professor, ProfessorAdmin)
+admin.site.register(Course, CourseAdmin)
+ 
+     
