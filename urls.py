5150aee0fff45051016e0db746b7bcb20b95de26
Yihui Chen <uninterrupted@gmail.com>
2012-04-16 21:30:43 -0400

    added some urls.

--- urls.py
+++ urls.py
@@ -1,5 +1,12 @@
+from obieconnect.views import *
 from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('',
-
+urlpatterns = patterns('',                  
+    url(r'^$', main_page),
+    
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', logout_page),
+    
+    url(r'^dept/([-\w]+)/$', dept_info),
+    url(r'^prof/([-\w]+)/$', prof_info),
 )
